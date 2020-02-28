b7bd8ccc4edd6c894dde3aa37d2a2a08bd7d2f03
syu0417 <s_kiba84@yahoo.co.jp>
2020-02-28 14:02:43 +0900

    シーン遷移の基盤作成

---
+++ ShiotanGame/Assets/Scenes/SceneManager/SceneTransition.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField, Header("シーン遷移実行フラグ")]
+    bool TransitionRun = false;
+
+    [SerializeField, Header("遷移先シーン名")]
+    string NextSceneName;
+
+    [SerializeField, Header("1つ前のシーン")]
+    string BeforSceneName;
+
+    private bool FadeStart;//フェード開始フラグ
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TransitionRun)
+        {
+            FadeStart = true;//フェード開始
+        }
+        if(FadeStart)
+        {
+            if(FeadOut())//フェードアウト->遷移
+            {
+                SceneTrans(NextSceneName);
+            }
+        }
+    }
+
+    private void SceneTrans(string scenename)//シーン遷移
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    private bool FeadOut()//フェード終了ならtrue,終了していなければfalseを返す
+    {
+        //フェードアウト実行
+        return false;
+    }
+
+    public void SetTransitionRun()
+    {
+        //遷移実行フラグをtrueに
+        TransitionRun = true;
+    }
+}
