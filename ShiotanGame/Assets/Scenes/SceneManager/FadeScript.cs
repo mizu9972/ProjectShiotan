51dc87ba245e024dbf5fcf5379240711e906c06c
syu0417 <s_kiba84@yahoo.co.jp>
2020-03-12 16:10:39 +0900

    シーン遷移関数を実行するとフェードアウト→シーン遷移する処理の実装。それに伴って使用したUniRxの導入。

---
+++ ShiotanGame/Assets/Scenes/SceneManager/FadeScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //パネルのイメージを操作するのに必要
+
+public class FadeScript : MonoBehaviour
+{
+
+    float fadeSpeed = 0.005f;        //透明度が変わるスピードを管理
+    float red, green, blue, alfa;   //パネルの色、不透明度を管理
+
+    public bool isFadeOut = false;  //フェードアウト処理の開始、完了を管理するフラグ
+    public bool isFadeIn = false;   //フェードイン処理の開始、完了を管理するフラグ
+
+    Image fadeImage;                //透明度を変更するパネルのイメージ
+
+    private bool FadeSts = false;//trueでフェード終了 
+    void Start()
+    {
+        fadeImage = GetComponent<Image>();
+        red = fadeImage.color.r;
+        green = fadeImage.color.g;
+        blue = fadeImage.color.b;
+        alfa = fadeImage.color.a;
+    }
+
+    void Update()
+    {
+        if (isFadeIn)
+        {
+            FadeSts = StartFadeIn();
+        }
+
+        if (isFadeOut)
+        {
+            FadeSts = StartFadeOut();
+        }
+    }
+
+    bool StartFadeIn()
+    {
+        alfa -= Mathf.Sin(fadeSpeed);               //a)不透明度を徐々に下げる
+        SetAlpha();                      //b)変更した不透明度パネルに反映する
+        if (alfa <= 0)
+        {                    //c)完全に透明になったら処理を抜ける
+            isFadeIn = false;
+            fadeImage.enabled = false;    //d)パネルの表示をオフにする
+            return true;
+        }
+        return false;
+    }
+
+    bool StartFadeOut()
+    {
+        fadeImage.enabled = true;  // a)パネルの表示をオンにする
+        alfa += Mathf.Sin(fadeSpeed);         // b)不透明度を徐々にあげる
+        SetAlpha();               // c)変更した透明度をパネルに反映する
+        if (alfa >= 1)
+        {             // d)完全に不透明になったら処理を抜ける
+            isFadeOut = false;
+            return true;
+        }
+        return false;
+    }
+
+    void SetAlpha()
+    {
+        fadeImage.color = new Color(red, green, blue, alfa);
+    }
+
+
+    public void SetIsFeadOut()//フェードアウトスタートをセット
+    {
+        alfa = 0.0f;
+        isFadeOut = true;
+        FadeSts = false;
+    }
+    public void SetIsFeadIn()//フェードアウトスタートをセット
+    {
+        alfa = 1.0f;
+        isFadeIn = true;
+        FadeSts = false;
+    }
+
+    public bool GetFeadStatus()
+    {
+        return FadeSts;
+    }
+}
--- ShiotanGame/Assets/Scenes/SceneManager/SceneTransition.cs
+++ ShiotanGame/Assets/Scenes/SceneManager/SceneTransition.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UniRx;
+using UniRx.Triggers;
 public class SceneTransition : MonoBehaviour
 {
     [SerializeField, Header("シーン遷移実行フラグ")]
     bool TransitionRun = false;
 
-    [SerializeField, Header("遷移先シーン名")]
-    string NextSceneName;
+    [Header("遷移先シーン名")]
+    public string NextSceneName;
 
-    [SerializeField, Header("1つ前のシーン")]
-    string BeforSceneName;
+    [Header("1つ前のシーン")]
+    public string BeforSceneName;
 
     private bool FadeStart;//フェード開始フラグ
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.UpdateAsObservable().Where(_ => FadeStart).Take(1).Subscribe(_ => this.GetComponent<FadeScript>().SetIsFeadOut());
     }
 
     // Update is called once per frame
@@ -44,7 +46,7 @@ public class SceneTransition : MonoBehaviour
     private bool FeadOut()//フェード終了ならtrue,終了していなければfalseを返す
     {
         //フェードアウト実行
-        return false;
+        return GetComponent<FadeScript>().GetFeadStatus();
     }
 
     public void SetTransitionRun()
