ca52a83594c79c53a17634bab64de7032e618992
ryusei <ryusey090@gmail.com>
2020-03-12 16:42:51 +0900

    移動処理（仮）　完成

--- ShiotanGame/Assets/Scenes/ProtoType/Script/ProtoMove2.cs
+++ ShiotanGame/Assets/Scenes/ProtoType/Script/ProtoMove2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProtoMove : MonoBehaviour
+public class ProtoMove2 : MonoBehaviour
 {
     [SerializeField]
     float waruspeed;    //スピード調整用
@@ -34,34 +34,41 @@ public class ProtoMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ブレーキ判断用
-        bool sts=false;
+        //回転の度合い
+        float step = 3.0f * Time.deltaTime;
 
         //現在の速度取得
         Vector3 Max = rb.velocity;
-        
-        //左回転
+
+        //左へ進む
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            // ワールド座標基準で、現在の回転量へ加算する
-            this.transform.Rotate(0.0f, -ang, 0.0f);
+            speed = Maxspeed;
+            //指定した方向にゆっくり回転する場合
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -90f, 0), step);
         }
 
-        //右回転
+        //右へ進む
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            // ワールド座標基準で、現在の回転量へ加算する
-            this.transform.Rotate(0.0f, ang, 0.0f);
+            speed = Maxspeed;
+            //指定した方向にゆっくり回転する場合
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90f, 0), step);
         }
 
-        //前進
+        //上へ進む
         if (Input.GetKey(KeyCode.UpArrow))
         {
             speed = Maxspeed;
+            //指定した方向にゆっくり回転する場合
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), step);
         }
-        else
+
+        //下へ進む
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            sts = true;
+            speed = Maxspeed;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 180.0f, 0), step);
         }
 
         //加速
@@ -70,7 +77,7 @@ public class ProtoMove : MonoBehaviour
             Nowkasoku += kasoku;
 
             //限界加速度制限
-            if (MaxKasoku<Nowkasoku)
+            if (MaxKasoku < Nowkasoku)
             {
                 Nowkasoku = MaxKasoku;
             }
@@ -87,36 +94,24 @@ public class ProtoMove : MonoBehaviour
                 Nowkasoku = 0;
             }
         }
-
-        //ブレーキ
-        if (Input.GetKey(KeyCode.DownArrow))
+                
+        //減速処理（移動）
+        if (speed > 0.5f)
         {
-            // 物体のブレーキ
-            speed *= brake; // 3Dの場合
-            sts = false;
+            speed *= gensokuritu;
+        }
+        else
+        {
+            speed = 0;
         }
 
-        //減速するかどうか？（ブレーキしていない時＆＆十字キー↑押していないとき）
-        if (sts == true)
-        {
-            //減速処理（移動）
-            if (speed > 0.5f)
-            {
-                speed *= gensokuritu;
-            }
-            else
-            {
-                speed = 0;
-            }
-        }
-        
         //初速度速くするための変数
         float xspeed;
         float speedx = Max.x;
         float speedz = Max.z;
 
         //現在のスピードの大きいほう(X・Z方向)と最大スピードの差分を取得(初速度を速くする用)
-        if (speedx<speedz)
+        if (speedx < speedz)
         {
             xspeed = Maxspeed - Mathf.Abs(speedz);
         }
@@ -126,10 +121,10 @@ public class ProtoMove : MonoBehaviour
         }
 
         //スピード調整用
-        xspeed /=waruspeed;
+        xspeed /= waruspeed;
 
         //慣性での移動用
-        rb.AddForce(this.gameObject.transform.forward * (speed+Nowkasoku)* xspeed, ForceMode.Acceleration);
+        rb.AddForce(this.gameObject.transform.forward * (speed + Nowkasoku) * xspeed, ForceMode.Acceleration);
 
         //速度制限 (十字キーのスピード＋現在の加速度　を超えない)
         if ((speed + Nowkasoku) / 2 < Mathf.Abs(rb.velocity.x))
