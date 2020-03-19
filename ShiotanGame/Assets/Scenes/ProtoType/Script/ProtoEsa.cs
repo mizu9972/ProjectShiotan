c271dfb56e9cc6bf0799e34145917966c87b79c9
ryusei <ryusey090@gmail.com>
2020-03-19 14:09:40 +0900

    3/19 競合エラー　修正

--- ShiotanGame/Assets/Scenes/ProtoType/Script/ProtoEsa.cs
+++ ShiotanGame/Assets/Scenes/ProtoType/Script/ProtoEsa.cs
@@ -4,42 +4,46 @@ using UnityEngine;
 
 public class ProtoEsa : MonoBehaviour
 {
-    //餌のオブジェクト
+    //エサが消えるまでの時間　測る用の変数
+    private float wait;
+
+    [Header("エサのオブジェクト")]
     public GameObject EsaPrefab;
 
-    public float wait;
+    [Header("エサ消えるまでの時間")]
+    public float Destroytime;
 
+    [Header("エサ投げる力")]
     public float throwrange;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        //エサ消えるまでの時間
         if(wait>0)
         {
             wait += Time.deltaTime;
-            if(wait>5)
+            if(wait> Destroytime)
             {
                 wait = 0;
             }
         }
+
         //えさ投げる
         if (Input.GetKey(KeyCode.Space))
         {
             if (wait == 0.0f)
             {
                 var bulletInstance = Instantiate<GameObject>(EsaPrefab, this.transform.position, this.transform.rotation);
-<<<<<<< HEAD
-                bulletInstance.GetComponent<Rigidbody>().AddForce(this.transform.forward * 12, ForceMode.VelocityChange);
-                bulletInstance.tag = "Esa";     // ピラニアが追いかける対象の判別にタグが必要だったため追加　青木
-=======
                 bulletInstance.GetComponent<Rigidbody>().AddForce(this.transform.forward * throwrange, ForceMode.VelocityChange);
->>>>>>> d59eeb74cadfea57a06f5e7ffc41554da57d2777
+                bulletInstance.tag = "Esa";     // ピラニアが追いかける対象の判別にタグが必要だったため追加　青木
+                bulletInstance.GetComponent<Rigidbody>().AddForce(this.transform.forward * throwrange, ForceMode.VelocityChange);
                 Destroy(bulletInstance, 5.0f);
                 wait += Time.deltaTime;
             }
