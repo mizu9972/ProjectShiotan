bae18f1c26a616e44e5089c77751e0dd3af513f5
syu0417 <s_kiba84@yahoo.co.jp>
2020-03-19 16:05:34 +0900

    変更前、変更後のタグ名を設定してタグを動的に変更する処理のスクリプトを作成

---
+++ ShiotanGame/Assets/Script/ChangeTag/ChangeTag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTag : MonoBehaviour
+{
+    [Header("変更前のタグ名")]
+    public string BeforTag;
+    [Header("変更後のタグ名")]
+    public string AfterTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(BeforTag!=null)
+        {
+            this.tag = BeforTag;//最初のタグを設定
+        }
+    }
+
+    public void ChangeTagName()//タグ名を変更する
+    {
+        if(AfterTag!=null)
+        {
+            Debug.Log("Change");
+            this.tag = AfterTag;
+        }
+        else
+        {
+            Debug.Log("変更後のタグ名が設定されてません。");
+        }
+    }
+}
--- ShiotanGame/Assets/Script/PiranhaFlock/FlockDiscoveryField.cs
+++ ShiotanGame/Assets/Script/PiranhaFlock/FlockDiscoveryField.cs
@@ -6,7 +6,8 @@ public class FlockDiscoveryField : MonoBehaviour
 {
     // ターゲットが群衆探索範囲に触れた時に呼ばれるメソッド
     private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player" || other.tag == "Esa") {
+        if (other.tag == "Player" || other.tag == "Esa")
+        {
             //if (!gameObject.transform.parent.GetComponent<FlockBase>().TargetObject) {
             gameObject.transform.parent.GetComponent<FlockBase>().DiscoveryTarget(other.gameObject);
             //}
