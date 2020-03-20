071d20c3752d8148ec2f8721a121d2fda770c79f
syu0417 <s_kiba84@yahoo.co.jp>
2020-03-20 23:57:47 +0900

    プロトからコピーしたシーンのオブジェクトを整理して、拡縮を反映させたいステージオブジェクトを1つにまとめ、拡大縮小を編集する処理の実装。 CameraTestシーンにあります。

---
+++ ShiotanGame/Assets/Scenes/CameraTest/StageManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField, Header("ステージの幅")]
+    private float StageWidth = 1.0f;
+
+    [SerializeField, Header("ステージの幅")]
+    private float StageDepth = 1.0f;
+
+    private Transform MyTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyTrans = this.GetComponent<Transform>();
+        MyTrans.localScale = new Vector3(StageWidth, 1.0f, StageDepth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyTrans.localScale = new Vector3(StageWidth, 1.0f, StageDepth);
+    }
+}
