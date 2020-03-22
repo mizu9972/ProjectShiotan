90bfec0518796045ea0e41c00e88ef47563a2467
mizu9972 <oisiimizu9972@icloud.com>
2020-03-22 16:28:33 +0900

    StageをstaticにしてNavMeshをベイクしてプレイヤーを追いかけるオブジェクトを追加しました

---
+++ ShiotanGame/Assets/Scenes/ProtoType/Script/EnemyNav.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour
+{
+    [SerializeField, Header("GameManager")]
+    GameObject m_GameManager = null;
+
+    NavMeshAgent m_NavAgent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_NavAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Chase();
+    }
+
+    //プレイヤーを追いかける
+    private void Chase()
+    {
+        var Player_ = m_GameManager.GetComponent<GameManagerScript>().Player;
+        if (Player_ != null)
+        {
+            m_NavAgent.destination = Player_.transform.position;
+        }
+    }
+}
