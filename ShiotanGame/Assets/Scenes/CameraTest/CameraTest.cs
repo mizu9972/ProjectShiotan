488698ddb9e735e324993402d123f024a6361e31
syu0417 <s_kiba84@yahoo.co.jp>
2020-03-24 22:42:00 +0900

    カメラに移動範囲制限とターゲット追従処理の追加。できる限り数値で設定できるようにしています。

---
+++ ShiotanGame/Assets/Scenes/CameraTest/CameraTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour
+{
+    [SerializeField]
+    Camera PlayerCamera;
+
+    [Header("追従するターゲット")]
+    public GameObject Target;
+
+    [Header("移動範囲(指定した値の正負が移動範囲になります)")]
+    public Vector3 Range = new Vector3(10.0f,50.0f,10.0f);
+
+    [Header("ターゲットからどれだけ離れるか")]
+    public Vector3 Distance = new Vector3(0.0f, 0.0f, 10.0f);
+    //[SerializeField,Header("ステージオブジェクト")]
+    //GameObject StageObj;
+    [Header("追跡ON")]
+    public bool isChase = true;
+    [Header("移動制限ON")]
+    public bool isRestriction = true;
+
+    private Transform MyTrans;
+    void Start()
+    {
+        PlayerCamera = this.GetComponent<Camera>();
+        MyTrans = PlayerCamera.transform;
+    }
+
+    void Update()
+    {
+        if(isChase)//追跡
+        {
+            ChaseTarget();
+        }
+        if(isRestriction)//移動範囲制限
+        {
+            MoveRestriction();
+        }
+    }
+
+    private void ChaseTarget()//ターゲット追跡
+    {
+        MyTrans.position = new Vector3(Target.transform.position.x + Distance.x,
+                                       MyTrans.position.y + Distance.y,
+                                       Target.transform.position.z + Distance.z);
+    }
+
+    private void MoveRestriction()//移動制限
+    {
+        MyTrans.position = new Vector3(Mathf.Clamp(MyTrans.position.x,-Range.x,Range.x),
+                                       Mathf.Clamp(MyTrans.position.y, -Range.y, Range.y),
+                                       Mathf.Clamp(MyTrans.position.z, -Range.z, Range.z));
+    }
+}
--- ShiotanGame/Assets/Scenes/CameraTest/StageManager.cs
+++ ShiotanGame/Assets/Scenes/CameraTest/StageManager.cs
@@ -1,26 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UniRx;
+using UniRx.Triggers;
 public class StageManager : MonoBehaviour
 {
     [SerializeField, Header("ステージの幅")]
-    private float StageWidth = 1.0f;
+    private FloatReactiveProperty StageWidth = new FloatReactiveProperty(1.0f);
 
-    [SerializeField, Header("ステージの幅")]
-    private float StageDepth = 1.0f;
+    [SerializeField, Header("ステージの奥行き")]
+    private FloatReactiveProperty StageDepth = new FloatReactiveProperty(1.0f);
 
+    [SerializeField,Header("Transform")]
     private Transform MyTrans;
+    
     // Start is called before the first frame update
     void Start()
     {
-        MyTrans = this.GetComponent<Transform>();
-        MyTrans.localScale = new Vector3(StageWidth, 1.0f, StageDepth);
+        //値の変更を検知して更新処理を実行するストリームの登録
+        StageWidth.Subscribe(_ => StageUpdate());
+        StageDepth.Subscribe(_ => StageUpdate());
+
+        if (!MyTrans)
+        {
+            MyTrans = this.GetComponent<Transform>();
+        }
+        MyTrans.localScale = new Vector3(StageWidth.Value, 1.0f, StageDepth.Value);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MyTrans.localScale = new Vector3(StageWidth, 1.0f, StageDepth);
+        
+    }
+
+    [ContextMenu("StageUpdate")]
+    void StageUpdate()
+    {
+        MyTrans.localScale = new Vector3(StageWidth.Value, 1.0f, StageDepth.Value);
     }
 }
