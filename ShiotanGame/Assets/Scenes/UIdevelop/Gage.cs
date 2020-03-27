0325602d075f9ed56913969f99e3d48ff44a9671
syu0417 <s_kiba84@yahoo.co.jp>
2020-03-27 17:00:47 +0900

    ゲージのUIを実装。外部から初期化関数とダメージ関数を呼び出してください。

---
+++ ShiotanGame/Assets/Scenes/UIdevelop/Gage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Gage : MonoBehaviour
+{
+    [Header("アニメーション時間")]
+    public float AnimationTime;
+    RectTransform MyRectTrans;
+
+    [SerializeField]
+    float MaxHP;
+
+    [SerializeField]
+    float NowHP;
+
+    void Start()
+    {
+        MyRectTrans = this.GetComponent<RectTransform>();  
+    }
+
+    public void InitGage(float Hp)//ヒットポイントの初期化
+    {
+        MaxHP = Hp;
+        NowHP = Hp;
+    }
+
+    public void Damage(float Hp)//ダメージ受ける
+    {
+        NowHP = Hp;//現在のHPを引数から取得
+        //現在のHPと最大HPの割合でゲージのアニメーションを行う
+        MyRectTrans.DOScale(new Vector3((NowHP/MaxHP),1,1), AnimationTime).SetEase(Ease.Linear);
+    }
+}
