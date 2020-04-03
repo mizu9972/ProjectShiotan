003590ae2493f88ae9befde0aab05ae5ce805298
AokiHyuga <52559533+hyuga0305@users.noreply.github.com>
2020-04-03 15:28:30 +0900

    ピラニア群衆の変更(Flock Prefab) ・Aiの更新 ・HumanoidBaseのコンポーネントを追加
    HumanoidBase.cs
    ・Getter/Setterの更新

--- ShiotanGame/Assets/Script/PiranhaFlock/FlockBase.cs
+++ ShiotanGame/Assets/Script/PiranhaFlock/FlockBase.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlockBase : MonoBehaviour {
-    public GameObject Piranha;      // ピラニアオブジェクトを入れるもの（なくてもできるけど一応配置してる）
-    public int PiranihaCount = 0;   // プランナーがレベルデザインやりやすい用
-    public List<GameObject> ChildPiranha;   // スクリプトで管理しないと、子の検索をかけた時に全ての子を返してくれないことがあったのでこういう設計にしています。
 
-    [SerializeField] private Vector3 InstantPositionCorrct;     // ピラニア生成の座標の誤差をどこまで設定しますか?
+    [SerializeField, Header("ピラニアオブジェクト")]
+    private GameObject Piranha;      // ピラニアオブジェクトを入れるもの（なくてもできるけど一応配置してる）
 
-    private Vector3 InitPos;        // 初期位置。住処の場所って認識でいいと思う
-    [SerializeField] private float MoveSpeed = 10.0f;
-    public int AttackPower = 0;
+    [SerializeField, Header("ピラニア生成数")]
+    private int PiranihaCount = 0;   // プランナーがレベルデザインやりやすい用
 
-    [SerializeField] private int PiranhaChaceDirayFrame = 5;
-    [SerializeField] private List<Vector3> TargetPosList;
+    [HideInInspector] public List<GameObject> ChildPiranha;   // スクリプトで管理しないと、子の検索をかけた時に全ての子を返してくれないことがあったのでこういう設計にしています。
 
-    public GameObject TargetObject; 
-    public bool IsChase = false;
+    [SerializeField, Header("ピラニア生成の生み出す誤差")]
+    private Vector3 InstantPositionCorrct;     // ピラニア生成の座標の誤差をどこまで設定しますか?
 
     private void Awake()
     {
@@ -25,7 +21,8 @@ public class FlockBase : MonoBehaviour {
         for (int i = 0; i < PiranihaCount; i++) {
             Vector3 CreatePos = new Vector3(Random.Range(-InstantPositionCorrct.x, InstantPositionCorrct.x), Random.Range(-InstantPositionCorrct.y, InstantPositionCorrct.y), Random.Range(-InstantPositionCorrct.z, InstantPositionCorrct.z));
             GameObject newObj = Instantiate(Piranha, gameObject.transform.position + CreatePos, Quaternion.identity, gameObject.transform);
-            newObj.transform.localScale = new Vector3(1 / gameObject.transform.localScale.x, 1 / gameObject.transform.localScale.y, 1 / gameObject.transform.localScale.z);
+            newObj.transform.localScale = new Vector3(1 / gameObject.transform.localScale.x, 1 / gameObject.transform.localScale.y, 1 / gameObject.transform.localScale.z); // サイズ指定
+
             // ToDo::ピラニアの動き方等もここで設定したい
             ChildPiranha.Add(newObj);
         }
@@ -33,86 +30,25 @@ public class FlockBase : MonoBehaviour {
 
     void Start() 
     {
-        InitPos = gameObject.transform.position;
     }
 
     void Update() 
     {
-        // ToDo::群衆の処理を行う
-
-        // ターゲットが存在するときにターゲットの座標を取得しておく
-        if (IsChase) {
-            // ディレイフレームを超え始めるとターゲットを追尾し、最初の座標を削除していく
-            if (TargetPosList.Count > PiranhaChaceDirayFrame) {
-                TargetPosList.RemoveAt(0);
-                ChaseTarget();
-
-                // もし、ついていたら追いかけずに攻撃する
-                if (IsChase) {
-                }
-                else {
-                    AttackTarget();
-                }
-            }
-            if(TargetObject)
-            {
-                TargetPosList.Add(TargetObject.transform.position);
-            }
-        }
-        // ターゲットを追っていないときの処理
-        else {
-            // ToDo::住処に戻る処理
-            gameObject.GetComponent<Rigidbody>().velocity = transform.forward * 0.0f;  // 動きを止める処理
-        }
-
+        // 群衆AIの処理を行う
+        gameObject.GetComponent<AIFlock>().AIUpdate();
 
         // 各ピラニアの動き
         if (ChildPiranha.Count > 0) {
             // 子のすべての動きをここで再現させる
             foreach (GameObject obj in ChildPiranha) {
+                // とりあえずピラニアのアップデートを掛ける
+                obj.GetComponent<PiranhaBase>().PiranhaUpdate();
+
                 // ToDo::追いかけ状態かそうでないかで動きが変わるかも？
-                if (IsChase) {
-
-                }
-                else {
-
-                }
             }
         }
         else {
             Debug.LogWarning(gameObject.transform.name + "にはピラニアが1匹もいません。");
         }
-    }
-
-    /// <summary>
-    /// ターゲット追尾
-    /// </summary>
-    private void ChaseTarget() 
-    {
-        gameObject.transform.LookAt(TargetPosList[0]);  // ターゲットの方向を向く
-        Vector3 newAngle = gameObject.transform.localEulerAngles;
-        newAngle.x = newAngle.z = 0.0f;
-        gameObject.transform.localEulerAngles = newAngle;
-        gameObject.GetComponent<Rigidbody>().velocity = transform.forward * MoveSpeed;  // 追尾
-        //gameObject.transform.Translate(transform.forward * MoveSpeed);    // 追尾
-    }
-
-    private void AttackTarget() 
-    {
-        
-    }
-
-    public void DiscoveryTarget(GameObject target) 
-    {
-        TargetObject = target;
-        IsChase = true;
-        TargetPosList.Clear();
-    }
-
-    public void LostTarget() 
-    {
-        TargetObject = null;
-        IsChase = false;
-        TargetPosList.Clear();
     }
 }
--- ShiotanGame/Assets/Script/PiranhaFlock/FlockDiscoveryField.cs
+++ ShiotanGame/Assets/Script/PiranhaFlock/FlockDiscoveryField.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class FlockDiscoveryField : MonoBehaviour
 {
+    [SerializeField] private List<string> TargetTag;
+
     // ターゲットが群衆探索範囲に触れた時に呼ばれるメソッド
     private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player" || other.tag == "Esa")
-        {
-            //if (!gameObject.transform.parent.GetComponent<FlockBase>().TargetObject) {
-            gameObject.transform.parent.GetComponent<FlockBase>().DiscoveryTarget(other.gameObject);
-            //}
+
+        // タグの検索
+        bool IsFind = false;
+        foreach(string Tag in TargetTag) {
+            if(Tag == other.tag) {
+                IsFind = true;
+                break;
+            }
         }
+
+        if (IsFind) {
+            // ターゲットが既にリストにないかをチェック
+            bool IsAdd = true;
+            foreach(GameObject Target in gameObject.transform.parent.GetComponent<AIFlock>().TargetList) {
+                if(Target == other.gameObject) {
+                    IsAdd = false;
+                    break;
+                }
+            }
+
+            // かぶっていなければ追加する
+            if (IsAdd) {
+                gameObject.transform.parent.GetComponent<AIFlock>().TargetList.Add(other.gameObject);
+            }
+        }
+
+        //if (!gameObject.transform.parent.GetComponent<FlockBase>().TargetObject) {
+        //gameObject.transform.parent.GetComponent<FlockBase>().DiscoveryTarget(other.gameObject);
+        //}
     }
 }
