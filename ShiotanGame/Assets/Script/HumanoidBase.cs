45288bb0b0e6e5e87005e143390214fa23839292
AokiHyuga <52559533+hyuga0305@users.noreply.github.com>
2020-04-03 13:54:24 +0900

    HumanoidBase.csを追加 生き物系のHP,ATKのベースです

---
+++ ShiotanGame/Assets/Script/HumanoidBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanoidBase : MonoBehaviour 
+{
+    public float m_HP = 0;
+    public float m_AttackPower = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        if (m_HP < 0) {
+            Debug.LogWarning(gameObject.name + "のHPが0になっています。　設定してください");
+        }
+    }
+
+    /// <summary>
+    /// HPが0になっているかをチェック
+    /// </summary>
+    /// <returns>死んでいたらtrueを返す</returns>
+    public bool DeadCheck() {
+        // 死んでいたらtrueを返す
+        if (m_HP < 0) {
+            return true;
+        }
+        // 生きているときにfalseを返す
+        return false;
+    }
+}
