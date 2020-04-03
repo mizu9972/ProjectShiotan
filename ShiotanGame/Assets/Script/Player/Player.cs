39305ebfb0e9fde23f0feeddc68d9d3d3754f3a1
mizu9972 <oisiimizu9972@icloud.com>
2020-04-03 13:40:16 +0900

    playerから水面オブジェクトを取得して波紋を発生させる処理を追加。水面オブジェクト(SeaPlane)をプレハブ化

--- ShiotanGame/Assets/Script/Player/Player.cs
+++ ShiotanGame/Assets/Script/Player/Player.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UniRx;
+
 public class Player : MonoBehaviour
 {
+
+    [SerializeField, Header("水面")]
+    GameObject SeaPlane = null;
+    [SerializeField, Header("波形テクスチャ")]
+    Texture WaveTex = null;
+
+    [SerializeField, Header("波発生間隔")]
+    float WaveInterval = 1.0f;
+
+    private WavePlane m_WavePlaneScript = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SeaPlane = GameObject.FindGameObjectWithTag("SeaPlane");
+        m_WavePlaneScript = SeaPlane.GetComponent<WavePlane>();
+        Wave();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Wave()
+    {
+        Observable.Interval(System.TimeSpan.FromSeconds(WaveInterval))
+            .Subscribe(_ => m_WavePlaneScript.AwakeWave(this.transform, 0.1f, WaveTex));
     }
 }
