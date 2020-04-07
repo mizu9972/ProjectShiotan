28bf0065bcd6cef8aaf6f8f75f6c562dc8649dac
syu0417 <s_kiba84@yahoo.co.jp>
2020-04-07 13:28:13 +0900

    PlayerのプレハブにHPゲージと表示するキャンバスを追加。カメラの方向を向き続ける処理用のスクリプトを追加。

---
+++ ShiotanGame/Assets/Script/UI/GetCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GetCamera : MonoBehaviour
+{
+    Canvas MyCanvas = null;
+    Camera mainCamera = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyCanvas = this.GetComponent<Canvas>();
+        mainCamera = Camera.main;
+        MyCanvas.worldCamera = mainCamera;
+    }
+
+    public Canvas GetCanvas()
+    {
+        return MyCanvas;
+    }
+}
---
+++ ShiotanGame/Assets/Script/UI/LookCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCamera : MonoBehaviour
+{
+    private Transform MyTrans = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyTrans = this.GetComponent<Transform>();
+    }
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        MyTrans.rotation = Camera.main.transform.rotation;//カメラの方向を向く
+    }
+}
