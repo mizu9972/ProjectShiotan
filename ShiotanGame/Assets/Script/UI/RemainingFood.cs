23155a972469775ab0823b603e79cb7480b34206
syu0417 <s_kiba84@yahoo.co.jp>
2020-04-10 14:33:36 +0900

    残りエサ数表示用のスクリプトを作成。MasrerCubeのインポート

---
+++ ShiotanGame/Assets/Script/UI/RemainingFood.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemainingFood : MonoBehaviour
+{
+    [SerializeField,Header("残りエサの数")]
+    private float remainFoods;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
