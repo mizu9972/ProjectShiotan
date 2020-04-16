275991b28f4199a315b891ec8ce0be0e64695a6d
ryusei <ryusey090@gmail.com>
2020-04-16 15:59:28 +0900

    プレイヤー移動時こぐアニメーション　設定

---
+++ ShiotanGame/Assets/Script/Player/PlayerAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetAnimation(bool set)
+    {
+        //移動しているときtrue
+        _animator.SetBool("Move", set);
+    }
+}
