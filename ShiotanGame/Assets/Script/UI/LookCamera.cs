57e33437176b2e86b6dea1289efe865896a6c41b
syu0417 <s_kiba84@yahoo.co.jp>
2020-04-20 22:37:39 +0900

    群衆プレハブにHPゲージを実装。ゲージの減りのアニメーションスピードを1から0.1に変更。

--- ShiotanGame/Assets/Script/UI/LookCamera.cs
+++ ShiotanGame/Assets/Script/UI/LookCamera.cs
@@ -6,18 +6,26 @@ public class LookCamera : MonoBehaviour
 {
     private Transform MyTrans = null;
     private Vector3 work_Pos = Vector3.zero;
+    private Camera mainCamera = null;
+    public Transform parentTransform = null;
+    private float posy = 0;
     // Start is called before the first frame update
     void Start()
     {
         MyTrans = this.GetComponent<Transform>();
+        mainCamera = Camera.main;
     }
     
     // Update is called once per frame
     void LateUpdate()
     {
-        work_Pos = MyTrans.localPosition;
-        MyTrans.localPosition = new Vector3(0, 0, 0);
+
+        //MyTrans.eulerAngles = new Vector3(60, 0, 0);
+        posy = parentTransform.eulerAngles.y;
+        //MyTrans.LookAt(mainCamera.transform);
+        //posy = posy - MyTrans.eulerAngles.y;
         MyTrans.LookAt(Camera.main.transform.position);
-        MyTrans.localPosition = work_Pos;
+        posy = parentTransform.localEulerAngles.y;
+        MyTrans.localEulerAngles = new Vector3(MyTrans.localEulerAngles.x, -posy , MyTrans.localEulerAngles.z);
     }
 }
