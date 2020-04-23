2e1a7be3c2ec2533a7e00f2df140f1691e5c754f
syu0417 <s_kiba84@yahoo.co.jp>
2020-04-24 02:12:09 +0900

    UICanvasに残りのエサの数を描画するUIを追加。現在はテキストで代用中。

---
+++ ShiotanGame/Assets/Script/UI/DrawDig.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawDig : MonoBehaviour
+{
+    [SerializeField, Header("描画する数値")]
+    private int drawNum;
+
+    public Text textObj;
+
+    private float U = 0f;
+    private float V = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textObj.text = drawNum.ToString();
+    }
+
+    public void SetDrawNum(int num)//描画する数値をセット
+    {
+        drawNum = num;
+    }
+
+    private void SetUVPosition()
+    {
+        //this.GetComponent<>
+    }
+}
---
+++ ShiotanGame/Assets/Script/UI/RestFood.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+public class RestFood : MonoBehaviour
+{
+    [SerializeField,Header("残りエサの数")]
+    private float restFoods;
+
+    [SerializeField, Header("HPを犠牲にするか")]
+    private bool isSacrifi;
+
+    [Header("1の位の描画オブジェクト")]
+    public RawImage DigOne;
+
+    [Header("10の位の描画オブジェクト")]
+    public RawImage DigTen;
+
+    private GameObject PlayerObj=null;//プレイヤー
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.UpdateAsObservable().
+            Where(_ => restFoods > 0).
+            Take(1).
+            Subscribe(_ => isSacrifi = true);//残りエサ数が0になったらHP犠牲状態へ
+
+        this.UpdateAsObservable().Where(_=>!PlayerObj).Subscribe(_ => Init());//プレイヤーポップ後に初期化
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(PlayerObj)//Playerのオブジェクトが空でなければ実行
+        {
+            restFoods = PlayerObj.GetComponent<Player>().GetRestFood();
+        }
+        SetDigObjNum(); //描画する数値をセット
+    }
+
+    private void SetDigObjNum()
+    {
+        int setNum = (int)restFoods % 10;//1の位
+        DigOne.GetComponent<DrawDig>().SetDrawNum(setNum);
+
+        setNum = (int)restFoods / 10;//10の位
+        DigTen.GetComponent<DrawDig>().SetDrawNum(setNum);
+    }
+
+    private void Init()
+    {
+        PlayerObj = GameObject.FindGameObjectWithTag("Player");//Playerを取得
+    }
+}
