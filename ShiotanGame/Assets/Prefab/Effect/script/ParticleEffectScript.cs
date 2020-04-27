488923cbd4196631435304186315065d88ac92d2
mizu9972 <oisiimizu9972@icloud.com>
2020-04-27 21:57:40 +0900

    パーティクルシステムを再生・一時停止・停止させるスクリプト追加

---
+++ ShiotanGame/Assets/Prefab/Effect/script/ParticleEffectScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//パーティクルシステム制御クラス
+public class ParticleEffectScript : MonoBehaviour
+{
+    [SerializeField,Header("パーティクルシステム")]
+    protected GameObject ParticleSystemObject = null;
+    private ParticleSystem m_ParticleSystem = null;
+
+    private void Start()
+    {
+        m_ParticleSystem = ParticleSystemObject.GetComponent<ParticleSystem>();
+    }
+
+    //再生
+    [ContextMenu("Start")]
+    public void StartEffect()
+    {
+        if (m_ParticleSystem)
+        {
+            m_ParticleSystem.Play();
+        }
+    }
+
+    //一時停止
+    [ContextMenu("Pause")]
+    public void PauseEffect()
+    {
+        if (m_ParticleSystem.isPlaying)
+        {
+            m_ParticleSystem.Pause();
+        }
+    }
+
+    //停止
+    [ContextMenu("Stop")]
+    public void StopEffect()
+    {
+        if (m_ParticleSystem.isPlaying)
+        {
+            m_ParticleSystem.Stop();
+        }
+    }
+}
