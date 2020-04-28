baf1d7e7d419fea1d209fdbe7a4bac0d747cf344
mizu9972 <oisiimizu9972@icloud.com>
2020-04-28 16:10:41 +0900

    柵破壊時のエフェクト作成

--- ShiotanGame/Assets/Script/Gimmick/BiteWall/BiteWallBase.cs
+++ ShiotanGame/Assets/Script/Gimmick/BiteWall/BiteWallBase.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class BiteWallBase : MonoBehaviour
 {
+    [SerializeField, Header("破壊エフェクト")]
+    protected GameObject ParticleEffect = null;
+    private ParticleEffectScript m_ParEffScr = null;
+
+    private void Start()
+    {
+        m_ParEffScr = ParticleEffect.GetComponent<ParticleEffectScript>();
+        Instantiate(ParticleEffect, this.gameObject.transform.position, Quaternion.identity);
+    }
     // Update is called once per frame
     void Update()
     {
         // HPが0以下になったら削除
         if (gameObject.GetComponent<HumanoidBase>().DeadCheck()) {
-            Destroy(gameObject);
+
+            Dead();
         }
+    }
+
+    [ContextMenu("Dead")]
+    void Dead()
+    {
+        if (m_ParEffScr != null)
+        {
+            m_ParEffScr.StartEffect();
+        }
+
+        Destroy(gameObject);
     }
 }
