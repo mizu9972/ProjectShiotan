8d2ff51ad7fe2541fd9f3ac14a41cb3d88871082
syu0417 <s_kiba84@yahoo.co.jp>
2020-05-06 23:18:43 +0900

    UIDevelopシーンをステージセレクトの開発用シーンに変更。ステージ選択処理の基盤を作成

---
+++ ShiotanGame/Assets/Script/StageSelect/StageImage.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageImage : MonoBehaviour
+{
+    [Header("一つ前のオブジェクト")]
+    public Image PrevStage;
+
+    [Header("一つ後のオブジェクト")]
+    public Image NextStage;
+
+    [Header("遷移先シーン名")]
+    public string NextScene;
+
+    private bool isSelect = false;//現在選択状態か
+
+    //前後のオブジェクトがあるかの判定用
+    private bool isNextExist = false;
+    private bool isPrevExist = false;
+    private RectTransform MyRectTrans;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //前後にオブジェクトがあるかを判定
+        if(NextStage)
+        {
+            isNextExist = true;
+        }
+        if(PrevStage)
+        {
+            isPrevExist = true;
+        }
+
+        MyRectTrans = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetisSelect(bool select)//選択状態の設定
+    {
+        isSelect = select;
+    }
+
+    public void SetSize(Vector2 sizeselta)
+    {
+        MyRectTrans.sizeDelta = sizeselta;
+    }
+
+    public void SelectStage()//ステージを選択
+    {
+        Camera.main.GetComponent<SceneTransition>().SetTransitionRun(NextScene);
+    }
+
+    public Image GoNext()//次のオブジェクトを選択
+    {
+        NextStage.GetComponent<StageImage>().SetisSelect(true);
+        isSelect = false;
+        return NextStage;//次のステージオブジェクトを返す
+    }
+    public Image GoPerv()//前のオブジェクトを選択
+    {
+        PrevStage.GetComponent<StageImage>().SetisSelect(true);
+        isSelect = false;
+        return PrevStage;//前のステージオブジェクトを返す
+    }
+
+    public bool GetNextExist()
+    {
+        return isNextExist;
+    }
+    public bool GetPrevExist()
+    {
+        return isPrevExist;
+    }
+}
---
+++ ShiotanGame/Assets/Script/StageSelect/StageSelect.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StageSelect : MonoBehaviour
+{
+    [Header("オブジェクトの間隔")]
+    public float Space;
+
+    [Header("選択オブジェクトのサイズ")]
+    public Vector2 SelectedSize;
+
+    [Header("非選択オブジェクトのサイズ")]
+    public Vector2 NotSelectedSize;
+
+    [Header("最初に選択状態になるステージ")]
+    public Image FirstSelectStage;
+
+    private Image NowSelectObj;//現在選択中のオブジェクト
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<HorizontalLayoutGroup>().spacing = Space;//並べる間隔をセット
+        NowSelectObj = FirstSelectStage;
+        FirstSelectStage.GetComponent<StageImage>().SetisSelect(true);
+        
+        var childObject = this.GetComponentsInChildren<RectTransform>();
+        foreach(var item in childObject)//すべての子オブジェクトをデフォルトサイズに
+        {
+            item.sizeDelta = NotSelectedSize;
+        }
+        FirstSelectStage.GetComponent<StageImage>().SetSize(SelectedSize);//最初に選択するオブジェクトのサイズを大きく
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyInput();
+    }
+
+    private void KeyInput()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))//ひとつ前のオブジェクトを選択
+        {
+            if(NowSelectObj.GetComponent<StageImage>().GetPrevExist())//一つ前にオブジェクトがあれば
+            {
+                //非選択状態へ
+                NowSelectObj.GetComponent<StageImage>().SetisSelect(false);
+                NowSelectObj.GetComponent<StageImage>().SetSize(NotSelectedSize);
+                //選択オブジェクトの変更
+                NowSelectObj = NowSelectObj.GetComponent<StageImage>().GoPerv();
+                NowSelectObj.GetComponent<StageImage>().SetisSelect(true);
+                NowSelectObj.GetComponent<StageImage>().SetSize(SelectedSize);
+            }
+            
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))//一つ後にオブジェクトがあれば
+        {
+            if(NowSelectObj.GetComponent<StageImage>().GetNextExist())
+            {
+                //非選択状態へ
+                NowSelectObj.GetComponent<StageImage>().SetisSelect(false);
+                NowSelectObj.GetComponent<StageImage>().SetSize(NotSelectedSize);
+                //選択オブジェクトの変更
+                NowSelectObj = NowSelectObj.GetComponent<StageImage>().GoNext();
+                NowSelectObj.GetComponent<StageImage>().SetisSelect(true);
+                NowSelectObj.GetComponent<StageImage>().SetSize(SelectedSize);
+            }   
+        }
+        if(Input.GetKeyDown(KeyCode.Return))//ステージを決定
+        {
+            NowSelectObj.GetComponent<StageImage>().SelectStage();
+        }
+    }
+
+    [ContextMenu("間隔をセット")]
+    private void  SetSpace()
+    {
+        this.GetComponent<HorizontalLayoutGroup>().spacing = Space;//並べる間隔をセット
+    }
+}
