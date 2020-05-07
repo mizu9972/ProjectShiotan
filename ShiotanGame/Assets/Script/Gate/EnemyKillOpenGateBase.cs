e9c93964e1561f7bb34e67599b9a7a1371dcc7a4
AokiHyuga <52559533+hyuga0305@users.noreply.github.com>
2020-05-07 13:57:19 +0900

    敵を倒して開く門の更新

--- ShiotanGame/Assets/Script/Gate/EnemyKillOpenGateBase.cs
+++ ShiotanGame/Assets/Script/Gate/EnemyKillOpenGateBase.cs
@@ -7,9 +7,36 @@ public class EnemyKillOpenGateBase : MonoBehaviour
     [SerializeField,Header("倒す必要のある敵")]
     private List<GameObject> Enemy;
 
+    [SerializeField, Header("右扉")]
+    private GameObject RightDoor;
+    [SerializeField, Header("左扉")]
+    private GameObject LeftDoor;
+
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 OldScale = gameObject.transform.localScale;
+        gameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        if (RightDoor) {
+            Vector3 newScale = OldScale;
+            RightDoor.transform.localScale = newScale;
+            Vector3 newPos = gameObject.transform.localPosition;
+            newPos.x += -OldScale.x;
+            RightDoor.transform.position = newPos;
+        }
+        else {
+            Debug.LogWarning("右扉が設定されていません");
+        }
+        if (LeftDoor) {
+            Vector3 newScale = OldScale;
+            LeftDoor.transform.localScale = newScale;
+            Vector3 newPos = gameObject.transform.localPosition;
+            newPos.x += OldScale.x;
+            LeftDoor.transform.position = newPos;
+        }
+        else {
+            Debug.LogWarning("左扉が設定されていません");
+        }
     }
 
     // Update is called once per frame
