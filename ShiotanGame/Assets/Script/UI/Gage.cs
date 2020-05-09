ad84348e92df5645c0a80ecd9936bb4afbb1798d
syu0417 <s_kiba84@yahoo.co.jp>
2020-05-09 18:46:18 +0900

    UIのテクスチャのフォルダを整理。HPバーのテクスチャをスプライトエディタで編集して割り当て。それに伴ってゲージの色を変える処理の実装。

--- ShiotanGame/Assets/Script/UI/Gage.cs
+++ ShiotanGame/Assets/Script/UI/Gage.cs
@@ -16,6 +16,15 @@ public class Gage : MonoBehaviour
     [SerializeField]
     float NowHP;
 
+    [Header("HPゲージの色を変えるHP割合ライン")]
+    public float DangerZone = 0.2f;
+
+    [Header("十分にHPがあるときのバー")]
+    public Sprite GreenBar;
+
+    [Header("危険域のバー")]
+    public Sprite RedBar;
+
     private Image MyImg;
     private Color MyCol;
     void Start()
@@ -41,6 +50,18 @@ public class Gage : MonoBehaviour
         }
         //現在のHPと最大HPの割合でゲージのアニメーションを行う
         MyRectTrans.DOScale(new Vector3((NowHP/MaxHP),1,1), AnimationTime).SetEase(Ease.Linear);
-        
+        ChangeTexture();
+    }
+
+    private void ChangeTexture()//HPの割合でテクスチャを変更
+    {
+        if ((NowHP / MaxHP) <= DangerZone)
+        {
+            MyImg.sprite = RedBar;
+        }
+        else
+        {
+            MyImg.sprite = GreenBar;
+        }
     }
 }
