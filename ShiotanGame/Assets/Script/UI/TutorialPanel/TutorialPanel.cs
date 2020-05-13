82d9de3d17333c1b2764044bad395bd1d17cc6fe
syu0417 <s_kiba84@yahoo.co.jp>
2020-05-13 17:47:52 +0900

    チュートリアル用の触れたら操作UIを描画するオブジェクトの実装。エディタの画面サイズがStandaloneに戻されていたので修正

---
+++ ShiotanGame/Assets/Script/UI/TutorialPanel/TutorialPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// プレイヤーが触れた時に表示したいUIを表示するパネルにアタッチ
+/// プレイヤーがたどり着いたときにUIを表示したい場所にオブジェクトを配置してください。
+/// 例：流れの手前で連打ボタンを表示したいなら流れの手前にオブジェクトを配置
+/// </summary>
+public class TutorialPanel : MonoBehaviour
+{
+    [Header("表示するパネル")]
+    public Image DrawPanel;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DrawPanel.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DrawPanel.enabled = false;
+        }
+    }
+}
