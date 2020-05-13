fd9ca454ccedbb57bf3db6e6aa99dd65ae298c18
AokiHyuga <52559533+hyuga0305@users.noreply.github.com>
2020-05-13 16:04:33 +0900

    ピラニアがターゲットを見つけた時に「！」マークを表示するスクリプト、キャンバスを作成 後は画像をはめるだけ

---
+++ ShiotanGame/Assets/Script/UI/SurprisedMarkBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurprisedMarkBase : MonoBehaviour
+{
+    [SerializeField, Header("生成してから削除するまでの時間")]
+    private float DeleteTime;
+    private float Count = 0.0f; // 削除タイム
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DeleteTime <= Count) {
+            Destroy(gameObject);
+        }
+        Count += Time.deltaTime;
+    }
+}
