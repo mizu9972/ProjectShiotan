b75168a8f3a77d2aa3868e9e38d2ac474b0f6dbe
syu0417 <s_kiba84@yahoo.co.jp>
2020-05-13 17:24:38 +0900

    UIの位置、挙動の修正

---
+++ ShiotanGame/Assets/Script/UI/ChaseUITarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseUITarget : MonoBehaviour
+{
+    private RectTransform MyRect;
+    [Header("ターゲット")]
+    public RectTransform TargetRect;
+
+    [Header("Xの補正値")]
+    public float XCorrection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRect = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPosition();
+    }
+
+    private void SetPosition()
+    {
+        float x, y, z = 1f;
+
+        x = TargetRect.position.x+XCorrection;
+        y = MyRect.position.y;
+        z = MyRect.position.z;
+
+        MyRect.position = new Vector3(x, y, z);
+    }
+}
