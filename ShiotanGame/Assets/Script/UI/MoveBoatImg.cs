5890582c700bbf75612119e53180174210fc39d0
syu0417 <s_kiba84@yahoo.co.jp>
2020-05-15 12:23:34 +0900

    HPゲージのボートの挙動修正

---
+++ ShiotanGame/Assets/Script/UI/MoveBoatImg.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBoatImg : MonoBehaviour
+{
+    [Header("開始オブジェクト")]
+    public RectTransform StartObj;
+
+    [Header("終了オブジェクト")]
+    public RectTransform EndObj;
+
+    [Header("動かすオブジェクト")]
+    public RectTransform MoveObj;
+
+    [Header("HPゲージ")]
+    public GameObject Gage;
+
+    private float Proportion = 0f;
+    private float YPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        YPos = MoveObj.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Proportion = Gage.GetComponent<Gage>().GetProportion();
+        Proportion = 1f - Proportion;
+        MoveObj.position = Vector3.Slerp(StartObj.position, EndObj.position, Proportion);
+        MoveObj.position = new Vector3(MoveObj.position.x, YPos, MoveObj.position.z);
+    }
+}
