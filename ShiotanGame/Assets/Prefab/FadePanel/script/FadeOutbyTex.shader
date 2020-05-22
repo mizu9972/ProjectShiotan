742913cadeb72149d74eb048db9e7b8345df7740
mizu9972 <oisiimizu9972@icloud.com>
2020-05-22 17:50:21 +0900

    テクスチャ情報によってフェードインアウトさせるスクリプト作成

---
+++ ShiotanGame/Assets/Prefab/FadePanel/script/FadeOutbyTex.shader
@@ -0,0 +1,79 @@
+﻿Shader "Unlit/FadeOutbyTex"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_TransitionTex("トランジション画像",2D) = "white"{}
+		_MinColor("色のしきい地",Float) = 0
+		_FadeSpeed("フェード速度",Float) = 1
+		_isActive("有効か",Float) = 0
+	}
+		SubShader
+		{
+			Tags { "RenderType" = "Opaque" }
+			LOD 100
+
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				// make fog work
+				#pragma multi_compile_fog
+
+				#include "UnityCG.cginc"
+
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				struct v2f
+				{
+					float2 uv : TEXCOORD0;
+					UNITY_FOG_COORDS(1)
+					float4 vertex : SV_POSITION;
+				};
+
+				sampler2D _MainTex;
+				float4 _MainTex_ST;
+
+				sampler2D _TransitionTex;
+				float4 _TransitionTex_ST;
+
+				sampler2D _InitStateTex;
+				float4 _InitStateTex_ST;
+
+				uniform float4 _StartTime;//開始時間
+				uniform float _isActive;//有効かどうか
+
+				uniform float _minColor;//しきい値
+				uniform float _FadeSpeed;//フェード速度
+
+				v2f vert(appdata v)
+				{
+					v2f o;
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+					UNITY_TRANSFER_FOG(o,o.vertex);
+					_StartTime = tex2D(_InitStateTex, v.uv);
+					return o;
+				}
+
+				fixed4 frag(v2f i) : SV_Target
+				{
+					// sample the texture
+					fixed4 col = tex2D(_MainTex, i.uv);
+					fixed4 addcol = tex2D(_TransitionTex, i.uv);
+
+					addcol = step(((_Time.x - _StartTime.x) * _FadeSpeed),addcol * _isActive);
+					col = col * addcol;
+					// apply fog
+					UNITY_APPLY_FOG(i.fogCoord, col);
+					return col;
+				}
+				ENDCG
+			}
+		}
+}
---
+++ ShiotanGame/Assets/Prefab/FadePanel/script/FadebyTex.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FadebyTex : MonoBehaviour
+{
+    [SerializeField, Header("時間取得マテリアル")]
+    private Material InitMat = null;
+
+    [SerializeField, Header("フェードインマテリアル")]
+    private Material FadeInMat = null;
+
+    [SerializeField, Header("フェードアウトマテリアル")]
+    private Material FadeOutMat = null;
+
+    [SerializeField, Header("トランジションルール画像")]
+    private Texture TransitionTex = null;
+
+    [SerializeField, Header("フェード速度")]
+    private float FadeSpeed = 5.0f;
+
+    [Header("フェードアウトかどうか")]
+    public bool isFadeOut = true;
+
+    [SerializeField, Header("有効無効")]
+    private bool isActive = false;
+
+    Action<RenderTexture, RenderTexture> m_FadeFunction;
+
+    private RenderTexture m_SetState = null;
+    private Texture2D m_SetInitState = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_SetState = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Default);
+        m_SetInitState = new Texture2D(1, 1);
+        m_SetInitState.SetPixel(0, 0, new Color(0.0f, 0.0f, 0.0f, 1.0f));
+        m_SetInitState.Apply();
+
+        if (isActive)
+        {
+            m_FadeFunction = Fade_InOut_Function;
+        }
+        else
+        {
+            m_FadeFunction = NoFunction;
+        }
+        //シェーダー初期化
+        FadeInMat.SetTexture("_TransitionTex", TransitionTex);
+        FadeInMat.SetFloat("_FadeSpeed", FadeSpeed);
+
+        FadeOutMat.SetTexture("_TransitionTex", TransitionTex);
+        FadeOutMat.SetFloat("_FadeSpeed", FadeSpeed);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        m_FadeFunction.Invoke(source, destination);
+    }
+
+    //フェードさせる
+    private void Fade_InOut_Function(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, isFadeOut ? FadeOutMat : FadeInMat);
+    }
+
+    //何も処理しない
+    private void NoFunction(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination);
+    }
+
+    [ContextMenu("FeedInStart")]
+    public void StartFadeIn()
+    {
+        Graphics.Blit(m_SetInitState, m_SetState, InitMat);
+        FadeInMat.SetTexture("_InitStateTex", m_SetState);
+        FadeInMat.SetFloat("_isActive", 1);
+
+        m_FadeFunction = Fade_InOut_Function;
+    }
+
+    [ContextMenu("FeedOutStart")]
+    public void StartFadeOut()
+    {
+        Graphics.Blit(m_SetInitState, m_SetState, InitMat);
+        FadeOutMat.SetTexture("_InitStateTex", m_SetState);
+        FadeOutMat.SetFloat("_isActive", 1);
+
+        m_FadeFunction = Fade_InOut_Function;
+    }
+}
