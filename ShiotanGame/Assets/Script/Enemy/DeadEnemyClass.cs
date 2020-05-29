16969cad45650b3093ad9541ccfb5ca2158ade9b
AokiHyuga <52559533+hyuga0305@users.noreply.github.com>
2020-05-29 12:53:02 +0900

    バトルで敵が死んだ歳にSEが停止するように修正

--- ShiotanGame/Assets/Script/Enemy/DeadEnemyClass.cs
+++ ShiotanGame/Assets/Script/Enemy/DeadEnemyClass.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeadEnemyAnimation : MonoBehaviour
+public class DeadEnemyClass : MonoBehaviour
 {
+    [SerializeField, Header("モデル")]
+    private GameObject Model;
+
     // Start is called before the first frame update
     void Start()
     {
