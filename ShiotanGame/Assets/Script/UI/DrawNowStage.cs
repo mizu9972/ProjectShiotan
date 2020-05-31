7c44e9d0e67cc91992e4b3adc495ee5338c77140
syu0417 <s_kiba84@yahoo.co.jp>
2020-05-31 21:59:53 +0900

    ステージナンバー描画

---
+++ ShiotanGame/Assets/Script/UI/DrawNowStage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class DrawNowStage : MonoBehaviour
+{
+    private Text Mytext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mytext = this.GetComponent<Text>();
+        Mytext.text = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
