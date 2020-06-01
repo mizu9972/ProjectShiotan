2ecf5ff0bfae17c23ff8a4938e4be6fd20e3554c
AokiHyuga <52559533+hyuga0305@users.noreply.github.com>
2020-06-01 12:19:33 +0900

    BiteWallにピラニア、敵が近づいたときのみHPバーの表示実装

---
+++ ShiotanGame/Assets/Script/Gimmick/BiteWall/BiteWallShowHP.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiteWallShowHP : MonoBehaviour
+{
+    [SerializeField, Header("表示するHPバー")]
+    private GameObject ShowHPBar;
+    private List<GameObject> EnemyList;
+
+    private void Start() {
+        gameObject.transform.parent.GetComponent<EnemyHpBase>().enabled = false;
+        ShowHPBar.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.name == "AttackField") {
+            gameObject.transform.parent.GetComponent<EnemyHpBase>().enabled = true;
+            ShowHPBar.SetActive(true);
+            if (!EnemyList.Contains(other.gameObject)) {
+                EnemyList.Add(other.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        foreach(GameObject List in EnemyList) {
+            if(other.gameObject == List) {
+                EnemyList.Remove(other.gameObject);
+                break;
+            }
+        }
+        if(EnemyList.Count <= 0) {
+            gameObject.transform.parent.GetComponent<EnemyHpBase>().enabled = false;
+            ShowHPBar.SetActive(false);
+        }
+    }
+}
