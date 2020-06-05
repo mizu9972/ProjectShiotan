4fa261d44decdeeafc1cee68d37b932ec27d0dc0
syu0417 <s_kiba84@yahoo.co.jp>
2020-06-05 14:59:48 +0900

    UI操作関係をフェード中に行わないよう修正。ゲーム中からタイトル、ステージセレクトに戻る場合のフェード関係の繋ぎ方を修正

---
+++ ShiotanGame/Assets/Script/Title/SetTitleUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTitleUI : MonoBehaviour
+{
+    public TitleScene titleScene;
+    private bool isEnd=false;//処理が終わった時用
+    // Start is called before the first frame update
+    void Awake()
+    {
+        titleScene.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GameManager.Instance.GetisFade()&&!isEnd)
+        {
+            titleScene.enabled = true;
+            isEnd = true;
+        }
+    }
+}
