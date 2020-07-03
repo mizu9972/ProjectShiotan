7f2c6b314b1fa5f95021650ef67c0d3e850ca7e2
syu0417 <s_kiba84@yahoo.co.jp>
2020-07-03 14:59:33 +0900

    新規ステージセレクトの基盤を作成

---
+++ ShiotanGame/Assets/Scenes/newStageSelect/PlayAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartAnimation()
+    {
+        //アニメーション最初から再生
+        animator.Play("Move", 0, 0.0f);
+    }
+}
