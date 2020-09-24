8596159099d4928ce6840114f71bf7036c089c68
mizu9972 <oisiimizu9972@icloud.com>
2020-09-24 17:00:29 +0900

    設定したステージオブジェクトが自動でスクロールするようにしました

--- NegiShiotan/Assets/Prototype/Script/Stage/StageConveyorSystem.cs
+++ NegiShiotan/Assets/Prototype/Script/Stage/StageConveyorSystem.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class StageConveyorSystem : MonoBehaviour
 {
+    [SerializeField,Header("ステージオブジェクト群")]
+    private List<GameObject> StagePlaneList;
+
+    [Header("ステージの挙動についての設定")]
+    [Tooltip("ステージの基本移動速度")]
+    public float ScrollBaseSpeed = 1.0f;
+    [Space(10)]
+
+    private GameObject ActiveStageObject = null;
+    private float NowScrollSpeed;//ステージ移動速度
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NowScrollSpeed = ScrollBaseSpeed;
+
+        StageInit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        ActiveStageObject.transform.Translate(-1.0f * NowScrollSpeed, 0, 0);
+    }
+
+    //ステージ処理初期化
+    void StageInit()
+    {
+        ActiveStageObject = StagePlaneList[0];
     }
 }
