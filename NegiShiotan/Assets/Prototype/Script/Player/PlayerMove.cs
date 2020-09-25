d0af99fca07bc18ae2231476b6c3df5360cd2c8f
syu0417 <s_kiba84@yahoo.co.jp>
2020-09-25 12:14:58 +0900

    プレイヤーの移動基盤作成

---
+++ NegiShiotan/Assets/Prototype/Script/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("移動速度")]
+    public float Speed = 1f;
+    private float Zpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveFunc();   
+    }
+
+
+    private void MoveFunc()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Zpos += Speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Zpos -= Speed * Time.deltaTime;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, Zpos);
+    }
+}
