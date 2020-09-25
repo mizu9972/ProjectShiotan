5f7c3e0480fb75e2605581e8c2c1861f551a25d5
syu0417 <s_kiba84@yahoo.co.jp>
2020-09-25 13:16:21 +0900

    プレイヤーの移動範囲制限実装

---
+++ NegiShiotan/Assets/Prototype/Script/Player/HitCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCheck : MonoBehaviour
+{
+    [SerializeField, Header("衝突中か")]
+    private bool isHit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetisHit()//衝突中かを返す
+    {
+        return isHit;
+    }
+
+    private void OnTriggerStay(Collider other)//ヒットチェック
+    {
+        if(LayerMask.LayerToName(other.gameObject.layer) == "Player_Raft")
+        {
+            isHit = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Player_Raft")
+        {
+            isHit = false;
+        }
+    }
+}
--- NegiShiotan/Assets/Prototype/Script/Player/PlayerMove.cs
+++ NegiShiotan/Assets/Prototype/Script/Player/PlayerMove.cs
@@ -6,7 +6,15 @@ public class PlayerMove : MonoBehaviour
 {
     [Header("移動速度")]
     public float Speed = 1f;
+
+    [Header("左側の移動制限用コライダ")]
+    public HitCheck LeftCollider;
+
+    [Header("右側の移動制限用コライダ")]
+    public HitCheck RightCollider;
+
     private float Zpos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +30,17 @@ public class PlayerMove : MonoBehaviour
 
     private void MoveFunc()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        //←入力がある状態で左側が飛び出てなければ
+        if(Input.GetKey(KeyCode.LeftArrow) && LeftCollider.GetisHit())
         {
             Zpos += Speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.RightArrow))
+        if(Input.GetKey(KeyCode.RightArrow) && RightCollider.GetisHit())
         {
             Zpos -= Speed * Time.deltaTime;
         }
         transform.position = new Vector3(transform.position.x, transform.position.y, Zpos);
     }
+
+
 }
