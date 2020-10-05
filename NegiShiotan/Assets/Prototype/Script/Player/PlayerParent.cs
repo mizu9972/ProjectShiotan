a0c6b1170d72906abebe3a234d9ed5204af3ba7f
syu0417 <s_kiba84@yahoo.co.jp>
2020-10-05 12:49:22 +0900

    プレイヤーの親子関係を確定。プロト用シーンにプレイヤーを召喚

---
+++ NegiShiotan/Assets/Prototype/Script/Player/PlayerParent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParent : MonoBehaviour
+{
+    [Header("プレイヤー")]
+    public GameObject PlayerObj;
+
+    [Header("イカダ")]
+    public GameObject RaftObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //////////////////////////////////
+    // プレイヤーとイカダ取得用関数
+    //////////////////////////////////
+    public GameObject GetPlayer()
+    {
+        return PlayerObj;
+    }
+    public GameObject GetRaft()
+    {
+        return RaftObj;
+    }
+}
