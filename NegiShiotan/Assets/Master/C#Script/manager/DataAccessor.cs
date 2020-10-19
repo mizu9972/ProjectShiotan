08e98cc7ab859a12711f27a83137f64bcf8aab8d
syu0417 <s_kiba84@yahoo.co.jp>
2020-10-19 14:50:17 +0900

    プレイヤーのステータス管理用のスクリプトとチェックポイント通過時のアクセス用のスクリプトを作成

---
+++ NegiShiotan/Assets/Master/C#Script/manager/DataAccessor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataAccessor: MonoBehaviour
+{
+    [SerializeField, Header("コインの数")]
+    private int m_Coin;
+
+    [SerializeField, Header("残機")]
+    private int m_Remain;
+
+    #region Getter&Setter
+    public int Coin
+    {
+        get { return m_Coin; }
+        set { m_Coin = value; }
+    }
+    public int Remain
+    {
+        get { return m_Remain; }
+        set { m_Remain = value; }
+    }
+    #endregion
+}
