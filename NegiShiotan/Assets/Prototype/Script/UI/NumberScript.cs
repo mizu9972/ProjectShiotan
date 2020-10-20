d729fdfb0bf9e5908c44f068eea81c04df5a596d
ryusei <ryusey090@gmail.com>
2020-10-20 16:41:32 +0900

    UI　簡易なもの完成

---
+++ NegiShiotan/Assets/Prototype/Script/UI/NumberScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class NumberScript : MonoBehaviour
+{
+    [Header("数　表示画像")]
+    public RawImage SetNumber;
+
+    [Header("桁数")]
+    public int truss;
+
+    void Start()
+    {
+        float x, y, w, h = 0;
+        
+        x = 0.0f;
+        y = 0.5f;
+        w = 0.2f;
+        h = 0.5f;
+
+        //0　表示
+        SetNumber.uvRect = new Rect(x, y, w, h);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SetNumberDraw(int Number_num)
+    {
+        float x=0, y=0, w=0, h=0;
+
+        if (truss == 0)
+        {
+            //2桁目　計算に含まない
+            Number_num %= 10;
+
+            //数値のUVのX座標
+            x = Number_num % 5.0f / 5.0f;
+
+            //数値のUVのY座標
+            if (Number_num < 5)
+            {
+                y = 0.5f;
+            }
+            if (Number_num >= 5)
+            {
+                y = 0.0f;
+            }
+        }
+
+        if(truss==10)
+        {
+            Number_num = Number_num / 10;
+
+            //数値のUVのX座標
+            x = Number_num % 5.0f / 5.0f;
+
+            //数値のUVのY座標
+            if (Number_num < 5)
+            {
+                y = 0.5f;
+            }
+            if (Number_num >= 5)
+            {
+                y = 0.0f;
+            }
+        }
+
+        w = 0.2f;
+        h = 0.5f;
+
+        SetNumber.uvRect = new Rect(x, y, w, h);
+    }
+}
---
+++ NegiShiotan/Assets/Prototype/Script/UI/UIScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+public class UIScript : MonoBehaviour
+{
+    [Header("HPUI")]
+    public Image[] HPUI;
+
+    private int MAXHP;
+    private int HPnumber = 0;
+    private int BeforeHP = 0;
+
+    [Header("PLAYER")]
+    public Status UIStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MAXHP = UIStatus.GetMAXHP();
+        HPnumber = MAXHP;
+        BeforeHP = HPnumber;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HPnumber = UIStatus.GetHP();
+
+        //HP減少
+        if (HPnumber<BeforeHP)
+        {
+            while (BeforeHP != HPnumber)
+            {
+                BeforeHP--;
+                if (BeforeHP < 0)
+                {
+                    BeforeHP = 0;
+                    Debug.Log("break");
+                    break;
+                }
+                else
+                {
+                    HPUI[BeforeHP].enabled = false;
+                }
+            }
+        }
+
+        //HP増加
+        if(HPnumber>BeforeHP)
+        {
+            while (BeforeHP != HPnumber)
+            {
+                BeforeHP++;
+                if (BeforeHP > MAXHP)
+                {
+                    BeforeHP = MAXHP;
+                    Debug.Log("break");
+                    break;
+                }
+                else
+                {
+                    HPUI[BeforeHP-1].enabled = true;
+                }
+            }
+        }
+
+    }
+}
