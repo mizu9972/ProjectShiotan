a463e9872fbb0509da021111ead773aeb2ad2cb7
mizu9972 <oisiimizu9972@icloud.com>
2020-10-22 12:39:03 +0900

    チェックポイント通過時判定用スクリプト作成

---
+++ NegiShiotan/Assets/Master/C#Script/Stage/LineSystem/CheckPointLine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //PlayerPointと触れたら
+        if (other.gameObject.layer == 11)
+        {
+            
+        }
+    }
+}
