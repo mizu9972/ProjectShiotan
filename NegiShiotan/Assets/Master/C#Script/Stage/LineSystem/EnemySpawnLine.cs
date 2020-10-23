3831171dd6a14c7e801b83993b323a7ba3bfe94c
mizu9972 <oisiimizu9972@icloud.com>
2020-10-23 12:54:42 +0900

    敵スポーンシステム更新

--- NegiShiotan/Assets/Master/C#Script/Stage/LineSystem/EnemySpawnLine.cs
+++ NegiShiotan/Assets/Master/C#Script/Stage/LineSystem/EnemySpawnLine.cs
@@ -16,7 +16,7 @@ public class EnemySpawnLine : MonoBehaviour
     public int EreaCount = 9;
     [HideInInspector]
     private const int EreaCountCst = 9;
-    [HideInInspector]
+    [/*HideInInspector,*/SerializeField]
     public bool[] Erea = new bool[EreaCountCst];
 
     // Start is called before the first frame update
@@ -53,6 +53,7 @@ public class EnemySpawnLine : MonoBehaviour
             }
         }
     }
+
 }
 
 //エディター拡張
@@ -60,68 +61,108 @@ public class EnemySpawnLine : MonoBehaviour
 public class SpawnEreaSelecter : Editor
 {
     EnemySpawnLine m_Erea;
+
+    bool[] recordErea = new bool[9];
+    
+    private void OnEnable()
+    {
+
+    }
     public override void OnInspectorGUI()
     {
+ 
+        serializedObject.Update();
         base.OnInspectorGUI();
-        m_Erea = target as EnemySpawnLine;
+
+
+        m_Erea = (EnemySpawnLine)target;
+
+
+
+
+
+        EditorGUI.BeginChangeCheck();
+
 
         //敵スポーン位置の設定項目
+
         EditorGUILayout.BeginVertical(GUI.skin.box);
         EditorGUILayout.LabelField("敵スポーン位置");
         EditorGUILayout.BeginHorizontal();
-        m_Erea.Erea[0] = ToggleCheck(m_Erea.Erea[0]);
-        m_Erea.Erea[1] = ToggleCheck(m_Erea.Erea[1]);
-        m_Erea.Erea[2] = ToggleCheck(m_Erea.Erea[2]);
+        recordErea[0] = EditorGUILayout.Toggle("", m_Erea.Erea[0]);
+        recordErea[1] = EditorGUILayout.Toggle("", m_Erea.Erea[1]);
+        recordErea[2] = EditorGUILayout.Toggle("", m_Erea.Erea[2]);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        recordErea[3] = EditorGUILayout.Toggle("", m_Erea.Erea[3]);
+        recordErea[4] = EditorGUILayout.Toggle("", m_Erea.Erea[4]);
+        recordErea[5] = EditorGUILayout.Toggle("", m_Erea.Erea[5]);
 
         EditorGUILayout.EndHorizontal();
 
         EditorGUILayout.BeginHorizontal();
-        m_Erea.Erea[3] = ToggleCheck(m_Erea.Erea[3]);
-        m_Erea.Erea[4] = ToggleCheck(m_Erea.Erea[4]);
-        m_Erea.Erea[5] = ToggleCheck(m_Erea.Erea[5]);
-
-        EditorGUILayout.EndHorizontal();
-
-        EditorGUILayout.BeginHorizontal();
-        m_Erea.Erea[6] = ToggleCheck(m_Erea.Erea[6]);
-        m_Erea.Erea[7] = ToggleCheck(m_Erea.Erea[7]);
-        m_Erea.Erea[8] = ToggleCheck(m_Erea.Erea[8]);
+        recordErea[6] = EditorGUILayout.Toggle("", m_Erea.Erea[6]);
+        recordErea[7] = EditorGUILayout.Toggle("", m_Erea.Erea[7]);
+        recordErea[8] = EditorGUILayout.Toggle("", m_Erea.Erea[8]);
 
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.EndVertical();
 
-    }
-    
-    //トグルが変更されたかどうかを検知する
-    private bool ToggleCheck(bool Erea)
-    {
-        EditorGUI.BeginChangeCheck();
-
-        //表示
-        Erea = EditorGUILayout.Toggle("", Erea);
-
+        //変更されたら
         if (EditorGUI.EndChangeCheck())
         {
-            Erea = ToggleChange(Erea);
-        }
+            //Undoに保存
+            Undo.RecordObject(m_Erea, "ChangeErea");
 
-        return Erea;
+            //反映
+            for (int u = 0; u < 9; u++)
+            {
+                m_Erea.Erea[u] = recordErea[u];
+            }
+
+            EditorUtility.SetDirty(m_Erea);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }      
+
+        serializedObject.ApplyModifiedProperties();
+
+        
     }
+  
+    //一個だけ選べるようにする場合は下の処理を利用する
 
-    //トグルの内容を変更する
-    private bool ToggleChange(bool ChangeErea)
-    {
-        if(ChangeErea == false)
-        {
-            return false;
-        }
+    ////トグルが変更されたかどうかを検知する
+    //private bool ToggleCheck(bool Erea)
+    //{
+    //    EditorGUI.BeginChangeCheck();
 
-        //一旦全てfalseに
-        for(int EreaNum = 0;EreaNum < m_Erea.EreaCount; EreaNum++)
-        {
-            m_Erea.Erea[EreaNum] = false;
-        }
+    //    //表示
+    //    Erea = EditorGUILayout.Toggle("", Erea);
 
-        return true;
-    }
+    //    if (EditorGUI.EndChangeCheck())
+    //    {
+    //        Erea = ToggleChange(Erea);
+    //    }
+
+    //    return Erea;
+    //}
+
+    ////トグルの内容を変更する
+    //private bool ToggleChange(bool ChangeErea)
+    //{
+    //    if(ChangeErea == false)
+    //    {
+    //        return false;
+    //    }
+
+    //    //一旦全てfalseに
+    //    for(int EreaNum = 0;EreaNum < m_Erea.EreaCount; EreaNum++)
+    //    {
+    //        m_Erea.Erea[EreaNum] = false;
+    //    }
+
+    //    return true;
+    //}
 }
