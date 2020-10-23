20980cc693ffac6655a63279e0711d4c260b94b5
syu0417 <s_kiba84@yahoo.co.jp>
2020-10-23 12:09:58 +0900

    オーディオテスト用のシーンとスクリプトの追加。SEのドップラー効果のパラメータを変更可能に。

--- NegiShiotan/Assets/Master/C#Script/AudioManager/AudioScript/SEPlayer.cs
+++ NegiShiotan/Assets/Master/C#Script/AudioManager/AudioScript/SEPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 //AudioSourceがなければ追加
@@ -7,17 +8,41 @@ using UnityEngine;
 public class SEPlayer : MonoBehaviour
 {
     [Header("再生するSEデータ")]
-    public AudioClip SEData = null;
+    public AudioClip m_SEData = null;
 
-    private AudioSource audioSource = null;//自分のオーディオソース
+    [Header("ドップラー使用")]
+    public bool m_UseDoppler = false;
+
+    [Header("ドップラーレベル")]
+    [Range(0f, 5f)] public float m_DopplerLevel = 1f;
+
+
+    private AudioSource m_audioSource = null;//自分のオーディオソース
+    
     // Start is called before the first frame update
     void Awake()
     {
-        audioSource = this.GetComponent<AudioSource>();
+        m_audioSource = this.GetComponent<AudioSource>();
+        SetUseDoppler();
     }
 
     public void PlaySound()//SEプレイ関数
     {
-        audioSource.PlayOneShot(SEData);
+        m_audioSource.PlayOneShot(m_SEData);
+    }
+
+    private void SetUseDoppler()//ドップラー使用切り替え
+    {
+        m_audioSource.spatialBlend = 1f;
+        switch(m_UseDoppler)
+        {
+            case false:
+                m_audioSource.dopplerLevel = 0f;
+                break;
+
+            case true:
+                m_audioSource.dopplerLevel = m_DopplerLevel;
+                break;
+        }
     }
 }
