7bd36f19f62af72e4469d136b7712cc5cdca783c
syu0417 <s_kiba84@yahoo.co.jp>
2020-10-26 14:18:38 +0900

    メインのステージにBGM再生用プレハブの配置とstart()で再生するスクリプトを仮置き。

---
+++ NegiShiotan/Assets/Master/C#Script/AudioManager/AudioScript/SoundStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundStart : MonoBehaviour
+{
+    public BGMPlayer bgmPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgmPlayer.PlayBgm();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
