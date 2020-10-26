b764e279e0381f16ef58cb6134568384a766d53f
syu0417 <s_kiba84@yahoo.co.jp>
2020-10-26 10:35:38 +0900

    ゲームの初期化用スクリプトとマネージャ用のシーン呼び出し用のスクリプトを実装

---
+++ NegiShiotan/Assets/Master/C#Script/manager/LoadManagerScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadManagerScene : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBoot()
+    {
+        // 必ず配置するオブジェクト用のシーンを読み込む
+        string managerSceneName = "ManagerScene";
+        if (!SceneManager.GetSceneByName(managerSceneName).IsValid())
+        {
+            SceneManager.LoadScene(managerSceneName, LoadSceneMode.Additive);
+        }
+    }
+}
