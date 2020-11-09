37094d4abb37a75d5df094284a1a2d6996f3abd0
ryusei <ryusey090@gmail.com>
2020-11-09 14:47:18 +0900

    ピラニア処理　修正

--- NegiShiotan/Assets/Prototype/Script/Object/CoinScript.cs
+++ NegiShiotan/Assets/Prototype/Script/Object/CoinScript.cs
@@ -7,13 +7,13 @@ public class CoinScript : MonoBehaviour
     [Header("コイン増える数")]
     public int UpCoin;
 
-    [Header("コイン数管理マネージャー")]
-    public Status CoinStatus;
+    //コイン数管理マネージャー
+    private Status CoinStatus;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CoinStatus = GameObject.FindGameObjectWithTag("Status").GetComponent<Status>();
     }
 
     // Update is called once per frame
