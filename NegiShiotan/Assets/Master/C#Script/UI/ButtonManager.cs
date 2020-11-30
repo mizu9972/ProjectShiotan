4c51a3c2ec9a2e56f37d045fc42cbdd30d9ce163
mizu9972 <oisiimizu9972@icloud.com>
2020-11-30 22:46:00 +0900

    クリア時の選択画面作成

---
+++ NegiShiotan/Assets/Master/C#Script/UI/ButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField, Header("初期選択ボタン")]
+    private Button InitialSelevtButton = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(InitialSelevtButton != null)
+        {
+            InitialSelevtButton.Select();
+        }
+    }
+
+}
