cad6e297f90ebaf01dd3a23f9599f6ba87ea45d0
syu0417 <s_kiba84@yahoo.co.jp>
2020-12-03 14:31:24 +0900

    岩の砕けるエフェクトをRockプレハブに追加。Suizu_Calmだけプレハブの変更が適用されていない状態。

--- NegiShiotan/Assets/Prototype/Script/Object/RockScript.cs
+++ NegiShiotan/Assets/Prototype/Script/Object/RockScript.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UniRx;
+using UniRx.Triggers;
 public class RockScript : MonoBehaviour
 {
     [Header("HP減る量")]
     public int Damage;
+
+    [Header("エフェクト再生スクリプト")]
+    public ParticleEffectScript m_playPart = null;
+
+    [Header("モデルのメッシュ")]
+    public MeshRenderer m_ModelMesh = null;
 
     //HP管理マネージャー
     private Status HPStatus;
@@ -26,8 +33,16 @@ public class RockScript : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            HPStatus.DamageHP(Damage,true);
-            Destroy(this.gameObject);
+            m_ModelMesh.enabled = false;//岩モデルの描画を消す
+
+            m_playPart.StartEffect();//Effectを再生
+
+            HPStatus.DamageHP(Damage,true);//プレイヤーのHPを減少
+
+            //パーティクル再生が終わったら削除
+            this.UpdateAsObservable().
+                Where(_ => m_playPart.isStopped()).Take(1).
+                Subscribe(_ => Destroy(this.gameObject));
         }
     }
 }
