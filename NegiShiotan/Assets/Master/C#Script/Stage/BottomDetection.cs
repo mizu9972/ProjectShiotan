16d3abe207c08bcbe0058d2e423f0f0345bd58e6
syu0417 <s_kiba84@yahoo.co.jp>
2020-12-14 15:03:00 +0900

    滝から落ちる時のイカダのエフェクトオンオフ処理を実装

---
+++ NegiShiotan/Assets/Master/C#Script/Stage/BottomDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomDetection : MonoBehaviour
+{
+    private GameObject m_Player = null;
+    private SplashControl m_SplashControl = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Player= GameObject.FindGameObjectWithTag("Player");
+        m_SplashControl = m_Player.GetComponentInChildren<SplashControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            m_SplashControl.isActive = true;//イカダのエフェクト再生
+        }
+    }
+}
