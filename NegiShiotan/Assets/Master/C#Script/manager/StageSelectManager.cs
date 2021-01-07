f5fe6c4e357af3f0ad87150bc0c8324c5fb2157b
mizu9972 <oisiimizu9972@icloud.com>
2021-01-07 15:37:58 +0900

    ステージ数減少

--- NegiShiotan/Assets/Master/C#Script/manager/StageSelectManager.cs
+++ NegiShiotan/Assets/Master/C#Script/manager/StageSelectManager.cs
@@ -14,7 +14,7 @@ public enum StageSelectAction
 public class StageSelectManager : MonoBehaviour
 {
     [SerializeField, Header("ステージ個数")]
-    private const int STAGE_NUM = 11;
+    private const int STAGE_NUM = 8;
 
     [SerializeField, Header("ステージ選択マスオブジェクト群")]
     private List<GameObject> StageSquares = new List<GameObject>();
@@ -116,7 +116,7 @@ public class StageSelectManager : MonoBehaviour
             StageSeqCollection[StageSquares[0]] = m_PreNexStageWhile[0];
         }
 
-        //ステージ２～10
+        //ステージ途中
         for (int StageNum = 1; StageNum < STAGE_NUM - 1; StageNum++)
         {
             m_PreNexStageWhile[StageNum].PreWhile = StageWhiles[StageNum - 1];
@@ -127,10 +127,10 @@ public class StageSelectManager : MonoBehaviour
 
         //ステージエンディング
         {
-            m_PreNexStageWhile[10].PreWhile = StageWhiles[9];
-            m_PreNexStageWhile[10].NexWhile = null;
+            m_PreNexStageWhile[STAGE_NUM - 1].PreWhile = StageWhiles[STAGE_NUM - 2];
+            m_PreNexStageWhile[STAGE_NUM - 1].NexWhile = null;
 
-            StageSeqCollection[StageSquares[10]] = m_PreNexStageWhile[10];
+            StageSeqCollection[StageSquares[STAGE_NUM - 1]] = m_PreNexStageWhile[STAGE_NUM - 1];
         }
     }
 
